209936cf4b83fb7efba26e46369cc9f26f7bac36
Pedro Mac NH. <pedrobettiolabe@gmail.com>
2026-03-25 16:49:09 -0300

    fix: update FLUTTER_APPLICATION_PATH in configuration files and enhance Info.plist with new keys

--- CORDIS/ios/Flutter/flutter_export_environment.sh
+++ CORDIS/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/newheartbrasil/Documents/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/newheartbrasil/Documents/CORDIS/CORDIS"
+export "FLUTTER_APPLICATION_PATH=/Users/newheartbrasil/Documents/cipherApp/CORDIS"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
